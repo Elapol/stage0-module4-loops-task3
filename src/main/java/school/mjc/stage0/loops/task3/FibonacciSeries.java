2beb82afc36fc1302c926be6919c3183a4d72695
elyad <ella.dikovich@gmail.com>
2022-11-19 13:43:19 +0100

    ela1

--- src/main/java/school/mjc/stage0/loops/task3/FibonacciSeries.java
+++ src/main/java/school/mjc/stage0/loops/task3/FibonacciSeries.java
@@ -2,20 +2,20 @@ package school.mjc.stage0.loops.task3;
 
 public class FibonacciSeries {
     public static void main(String[] args) {
-        printFibonacci(11);
+        printFibonacci(5);
     }
 
     public static void printFibonacci(int lastFibonacci) {
-        int counter;
-        int f1 = 1;
+        int f1 = 0;
         int f2 = 1;
-            System.out.println(f1 + "\n" +f2);
-            for (counter = 3; counter <= lastFibonacci; counter++) {
-                int sum = f1 + f2;
-                System.out.println(sum);
-                f1 = f2;
-                f2 = sum;
-            }
+        int sum;
+        System.out.println(f1 + "\n" + f2);
+        for (int counter = 3; counter <= lastFibonacci; counter++) {
+         sum=f1+f2;
+         System.out.println(sum);
+         f1=f2;
+         f2=sum;
         }
     }
+}
 
